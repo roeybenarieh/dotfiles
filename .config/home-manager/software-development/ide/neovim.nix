bc663f07edf519d9b4d878545d1181bcfa046053
roey ben arieh <roey280404@gmail.com>
2024-11-01 12:31:21 +0200

    huck(nix): install packaged needed by neovim not managed with nix

--- .config/home-manager/software-development/ide/neovim.nix
+++ .config/home-manager/software-development/ide/neovim.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ pkgs.neovim pkgs.ripgrep pkgs.nixpkgs-fmt ];
+  home.packages = with pkgs; [ pkgs.neovim pkgs.ripgrep pkgs.nixpkgs-fmt pkgs.xclip pkgs.gcc ];
   home.sessionVariables = { EDITOR = "nvim"; };
   home.shellAliases = { n = "nvim"; };
 }
