5f7d2a8bbbdd94ac877895422eae556670cb68f2
roey ben arieh <roey280404@gmail.com>
2024-11-01 12:31:44 +0200

    feat(nix): remmina

--- .config/home-manager/software-development/netwoking.nix
+++ .config/home-manager/software-development/netwoking.nix
@@ -4,4 +4,9 @@
   home.packages = with pkgs; [ curl wget postman ];
   # sshx for sharing ssh sessions
   programs.ssh.enable = true;
+  # rdp(and other protocols) client
+  services.remmina = {
+    enable = true;
+    addRdpMimeTypeAssoc = true;
+  };
 }
