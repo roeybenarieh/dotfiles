5818eaa9bfc7b12816335c845349aebf7fa9c664
roey ben arieh <roey280404@gmail.com>
2024-11-12 10:49:08 +0200

    feat(nix): powershell

--- .config/home-manager/terminal/default.nix
+++ .config/home-manager/terminal/default.nix
@@ -3,8 +3,9 @@
 {
   imports = [ ./zsh.nix ./alacritty.nix ./tmux.nix ];
   home.packages = with pkgs;
-    [ pkgs.xclip ]; # better Ctrl+c Ctrl+v integration with terminal
+    [ xclip powershell ]; # better Ctrl+c Ctrl+v integration with terminal
   home.sessionVariables = {
     TERM = "alacritty";
   }; # use alacritty as default terminal
+  home.shellAliases = { powershell = "pwsh"; };
 }
