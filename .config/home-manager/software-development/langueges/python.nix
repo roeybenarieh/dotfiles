181f0a8d71e99618ccf785fc4dc15085db1d915a
roey ben arieh <roey280404@gmail.com>
2024-11-22 22:20:00 +0200

    feat(nix): better poetry config file

--- .config/home-manager/software-development/langueges/python.nix
+++ .config/home-manager/software-development/langueges/python.nix
@@ -3,11 +3,7 @@
 {
   home.packages = with pkgs; [ python312 ];
   programs.poetry = { enable = true; };
-  home.file = {
-    # TODO: change the path
-    "${config.home.homeDirectory}/.config/pypoetry/config.toml".source =
-      lib.mkForce ../../../../.config/pypoetry/config.toml;
-  };
+  xdg.configFile."pypoetry/config.toml".source = ../../../../.config/pypoetry/config.toml;
   home.sessionVariables = {
     PYTHONASYNCIODEBUG = 1; # for debuging asyncio application
   };
