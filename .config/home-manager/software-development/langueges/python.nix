fa6d9dfcf07e57b7d74d9849072eb63f722bf93d
roey ben arieh <roey280404@gmail.com>
2024-11-23 10:10:40 +0200

    fix(nix): install pip in addition to pyhon

--- .config/home-manager/software-development/langueges/python.nix
+++ .config/home-manager/software-development/langueges/python.nix
@@ -1,9 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [ python312 ];
+  # install python+pip
+  home.packages = with pkgs; [ python312 python312Packages.pip ];
+
+  # poetry
   programs.poetry = { enable = true; };
   xdg.configFile."pypoetry/config.toml".source = ../../../../.config/pypoetry/config.toml;
+
+  # python configuration
   home.sessionVariables = {
     PYTHONASYNCIODEBUG = 1; # for debuging asyncio application
   };
