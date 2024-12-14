1896d274756d97f4a2786ac8352afdcd3f4fe6da
roey ben arieh <roey280404@gmail.com>
2024-12-14 12:29:35 +0200

    feat(nix): fzf session variables

--- .config/home-manager/basic/fzf.nix
+++ .config/home-manager/basic/fzf.nix
@@ -2,4 +2,21 @@
 
 {
   programs.fzf.enable = true;
+  home.sessionVariables = {
+    # preview fzf files with the bat(batcat) cli
+    FZF_CTRL_T_OPTS = ''
+      --preview '${pkgs.bat}/bin/bat -n --color=always {}'
+       --bind 'ctrl-/:change-preview-window(down|hidden|)'
+       --color header:italic
+       --header 'CTRL-/ to change preview window'
+    '';
+    FZF_CTRL_R_OPTS = ''
+      --preview 'echo {}' --preview-window up:3:hidden:wrap
+      --bind 'ctrl-/:toggle-preview'
+      --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+      --color header:italic
+      --header 'CTRL=/ to change preview window. CTRL-Y to copy command into clipboard'
+    '';
+    FZF_ALT_C_OPTS = " --preview 'tree -C {}'";
+  };
 }
