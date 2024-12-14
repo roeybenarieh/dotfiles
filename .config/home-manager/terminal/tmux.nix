01cb6814e054908007c01db2b8b63278f08b5b5e
roey ben arieh <roey280404@gmail.com>
2024-12-14 12:29:46 +0200

    feat(nix): tmux session variables

--- .config/home-manager/terminal/tmux.nix
+++ .config/home-manager/terminal/tmux.nix
@@ -12,4 +12,11 @@
     source = ../../tmux;
     recursive = true;
   };
+  home.sessionVariables = {
+    ZSH_TMUX_UNICODE = "true"; # enforce utf-8 for showing shell icons
+    ZSH_TMUX_AUTOSTART = "true"; # start tmux when session start
+    ZSH_TMUX_FIXTERM = "true"; # set 256-color terminal if supported
+    ZSH_TMUX_AUTOCONNECT = "false"; # start tmux session every terminal loggin
+    ZSH_TMUX_CONFIG = "~/.config/tmux/tmux.conf";
+  };
 }
