4b2704f405f0434c7f7d62c2cf3391e8fcf422d7
roey ben arieh <roey280404@gmail.com>
2024-12-14 16:23:54 +0200

    feat(nix): thefuck command

--- .config/home-manager/terminal/default.nix
+++ .config/home-manager/terminal/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [ ./zsh.nix ./alacritty.nix ./tmux.nix ];
+  programs.thefuck.enable = true;
   home.packages = with pkgs;
     [ xclip powershell ]; # better Ctrl+c Ctrl+v integration with terminal
   home.sessionVariables = {
