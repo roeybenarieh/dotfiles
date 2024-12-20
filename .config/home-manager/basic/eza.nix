cd4f61d402297788d93fc7047d96be632adccd86
roey ben arieh <roey280404@gmail.com>
2024-12-20 23:47:53 +0200

    feat(nix): better eza configuration

--- .config/home-manager/basic/eza.nix
+++ .config/home-manager/basic/eza.nix
@@ -1,5 +1,5 @@
 # eza is a 'ls' alternative
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   programs.eza = {
@@ -7,16 +7,10 @@
     extraOptions = [
       "--git" # List each file's Git status if tracked or ignored
       "--icons" # Display icons next to file names
+      "--group" # Display the group of the resourcd
     ];
-    # allow shell integrations only if the shell itself is enabled
-    enableBashIntegration = config.programs.bash.enable;
-    enableZshIntegration = config.programs.zsh.enable;
-    enableFishIntegration = config.programs.fish.enable;
-    enableNushellIntegration = config.programs.nushell.enable;
-    enableIonIntegration = config.programs.ion.enable;
   };
   home.shellAliases = {
-    ls = "eza";
-    l = "ll";
+    l = "ls -lh"; # defaulty l = "ls -lFh", and -F doesnt exists in eza.
   };
 }
