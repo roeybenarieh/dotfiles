cd3aa7589ba177d2df064bc2e1986dabfe75ba20
roey ben arieh <roey280404@gmail.com>
2025-01-31 13:16:20 +0200

    feat(nix): make firefox deafult app for certain file types

--- .config/home-manager/browser/default.nix
+++ .config/home-manager/browser/default.nix
@@ -1,5 +1,18 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [ ./tor.nix ./chromium.nix ./firefox ];
+  # make firefox the default for opening things
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "application/pdf" = "firefox.desktop";
+      "image/jpeg" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
 }
