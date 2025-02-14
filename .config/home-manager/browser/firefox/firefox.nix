eb5f777f688192d040d28c3bb5129c858cae3ef0
roey ben arieh <roey280404@gmail.com>
2025-02-14 13:24:25 +0200

    fix(nix): cleanup firefox configuration

--- .config/home-manager/browser/firefox/firefox.nix
+++ .config/home-manager/browser/firefox/firefox.nix
@@ -4,6 +4,20 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
+
+    # Check about:policies#documentation for options.
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+    };
+
     profiles.roey = {
       isDefault = true; # set as the default profile
       settings = {
@@ -28,30 +42,6 @@
         "dom.security.https_only_mode" = true;
         # automaticly enable every extension
         "extensions.autoDisableScopes" = 0;
-        # Disable telemetry
-        # https://wiki.mozilla.org/Platform/Features/Telemetry
-        # https://wiki.mozilla.org/Privacy/Reviews/Telemetry
-        # https://wiki.mozilla.org/Telemetry
-        # https://www.mozilla.org/en-US/legal/privacy/firefox.html#telemetry
-        # https://support.mozilla.org/t5/Firefox-crashes/Mozilla-Crash-Reporter/ta-p/1715
-        # https://wiki.mozilla.org/Security/Reviews/Firefox6/ReviewNotes/telemetry
-        # https://gecko.readthedocs.io/en/latest/browser/experiments/experiments/manifest.html
-        # https://wiki.mozilla.org/Telemetry/Experiments
-        # https://support.mozilla.org/en-US/questions/1197144
-        # https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/telemetry/internals/preferences.html#id1
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.archive.enabled" = false;
-        "experiments.supported" = false;
-        "experiments.enabled" = false;
-        "experiments.manifest.uri" = "";
-        # Disable health reports (basically more telemetry)
-        # https://support.mozilla.org/en-US/kb/firefox-health-report-understand-your-browser-perf
-        # https://gecko.readthedocs.org/en/latest/toolkit/components/telemetry/telemetry/preferences.html
-        "datareporting.healthreport.uploadEnabled" = false;
-        "datareporting.healthreport.service.enabled" = false;
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "browser.urlbar.update2.engineAliasRefresh" = true;
       };
       # configure search engines
       extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
