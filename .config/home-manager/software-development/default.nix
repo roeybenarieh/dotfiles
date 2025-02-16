a740a448b3d5c76cda9e948ed27d7cb78bd4b0d3
roey ben arieh <roey280404@gmail.com>
2025-02-16 11:28:43 +0200

    feat(nix): cli programs for software development

--- .config/home-manager/software-development/default.nix
+++ .config/home-manager/software-development/default.nix
@@ -3,4 +3,13 @@
 {
   imports =
     [ ./ide ./source-control ./langueges ./netwoking.nix ./virtualization ];
+  home.packages = with pkgs; [
+    # debug programs
+    ltrace
+    strace
+
+    # archive management
+    zip
+    unzip
+  ];
 }
