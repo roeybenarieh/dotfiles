4229c3af430602dc2e38efc23a3ddd6f7c48b381
roey ben arieh <roey280404@gmail.com>
2025-02-22 22:09:46 +0200

    feat(nix): use bat extensions

--- .config/home-manager/basic/bat.nix
+++ .config/home-manager/basic/bat.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 
 {
-  programs.bat = { enable = true; };
-  # display man pages using bat
-  home.sessionVariables = {
-    MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
-    MANROFFOPT = "-c";
+  programs.bat = {
+    enable = true;
+    extraPackages = with pkgs.bat-extras; [
+      prettybat # pretty print files(format the files before printing)
+      batman # use bat to display man pages
+      batpipe # integrate bat with other tools
+    ];
   };
   home.shellAliases = {
     cat = "${pkgs.bat}/bin/bat";
