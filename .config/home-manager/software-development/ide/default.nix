3ce4b99d563ed487f6fa22f9671e5cf3acf76c96
roey ben arieh <roey280404@gmail.com>
2025-03-04 19:24:17 +0200

    chore: update flake.lock

--- .config/home-manager/software-development/ide/default.nix
+++ .config/home-manager/software-development/ide/default.nix
@@ -4,7 +4,7 @@
   imports = [ ./neovim.nix ];
   programs.vscode = {
     enable = true;
-    extensions = with pkgs; [
+    profiles.default.extensions = with pkgs; [
       vscode-extensions.ms-python.python # microsoft python extension
       vscode-extensions.esbenp.prettier-vscode # prettier formatter
       vscode-extensions.charliermarsh.ruff # python ruff linter
