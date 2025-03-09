4d8c602d775b0edcc5b5e3a2d362b8d69c2df04c
roey ben arieh <roey280404@gmail.com>
2025-03-09 07:56:36 +0200

    fix(nix): migrate terminal to snowfall

---
+++ nix/modules/home/cli/default.nix
@@ -0,0 +1,59 @@
+{ namespace, lib, config, ... }:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.tmux;
+
+
+  # TODO: get a better solution according to what will be decided in the github issue
+  # solution from: https://discourse.nixos.org/t/how-to-specify-programs-sqlite-for-command-not-found-from-flakes/22722/2
+  # more info:
+  # https://github.com/NixOS/nixpkgs/issues/171054
+  # https://discourse.nixos.org/t/command-not-found-not-working/24023/3
+  # https://www.youtube.com/watch?v=sLGoWAGklds
+  #
+  # To find the channel url I went in:
+  # https://channels.nixos.org/ > nixos-22.05 > nixexprs.tar.gz
+  # All the available channels you can browse https://releases.nixos.org
+  nixos_tarball = pkgs.fetchzip {
+    url = "https://releases.nixos.org/nixos/22.05/nixos-22.05.3737.3933d8bb912/nixexprs.tar.xz";
+    sha256 = "sha256-+xhJb0vxEAnF3hJ6BZ1cbndYKZwlqYJR/PWeJ3aVU8k=";
+  }
+  ;
+  # extract only the sqlite file
+  programs-sqlite = pkgs.runCommand "program.sqlite" { } ''
+    cp ${nixos_tarball}/programs.sqlite $out
+  '';
+in
+{
+  options.${namespace}.cli = with types; {
+    enable = mkBoolOpt false "Whether or not to enable cli utilities.";
+  };
+
+  config = mkIf cfg.enable {
+    programs = {
+      # helping fix up command syntax errors
+      thefuck.enable = true;
+
+      # autocompletion
+      carapace.enable = true;
+
+      command-not-found = {
+        enable = true;
+        dbPath = programs-sqlite;
+      };
+      nix-index = {
+        enable = false;
+      };
+    };
+    home.packages = with pkgs; [
+      xclip # better Ctrl+c Ctrl+v integration with terminal
+    ];
+    home.sessionVariables = {
+      # command-not-found variables
+      # TODO: make the auto installation work
+      NIX_AUTO_RUN = "y"; # when typing a program not currently installed, automaticly will be installed via nix-shell.
+      NIX_AUTO_RUN_INTERACTIVE = 1;
+    }; # use alacritty as default terminal
+  };
+}
---
+++ nix/modules/home/terminal/default.nix
@@ -0,0 +1,61 @@
+{ namespace, lib, config, ... }:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.terminal;
+in
+{
+  options.${namespace}.terminal = with types; {
+    enable = mkBoolOpt false "Whether or not to enable terminal.";
+  };
+
+  config = mkIf cfg.enable {
+    # install nerdfont
+    # run 'fc-cache -rf' when changing/installing fonts
+    home.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
+
+    # install alacritty
+    programs.alacritty = {
+      enable = true;
+
+      # alacritty configuration file
+      settings = {
+        window = {
+          padding = {
+            x = 4;
+            y = 8;
+          };
+          decorations = "full";
+          startup_mode = "Windowed";
+          title = "Alacritty";
+          dynamic_title = true;
+          decorations_theme_variant = "None";
+        };
+
+        font =
+          let
+            jetbrainsMono = style: {
+              family = "JetBrainsMono Nerd Font";
+              inherit style;
+            };
+          in
+          {
+            # size = 15;
+            # normal = jetbrainsMono "Regular";
+            bold = jetbrainsMono "Bold";
+            italic = jetbrainsMono "Italic";
+            bold_italic = jetbrainsMono "Bold Italic";
+          };
+
+        cursor = { style = "Block"; };
+
+        env = { TERM = "xterm-256color"; };
+
+        general.live_config_reload = true;
+      };
+    };
+    home.sessionVariables = {
+      TERM = "alacritty";
+    };
+  };
+}
