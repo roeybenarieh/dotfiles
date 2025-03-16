1a96d0ccce687688de34211bb489a737d743b265
roey ben arieh <roey280404@gmail.com>
2025-03-16 09:24:04 +0200

    fix(home-computer): fix gpu settings, no more crashes at power up

--- nix/modules/nixos/gpu/nvidia1080ti/default.nix
+++ nix/modules/nixos/gpu/nvidia1080ti/default.nix
@@ -17,6 +17,11 @@ in
     hardware.nvidia = {
       open = false;
       modesetting.enable = true;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
     environment.systemPackages = with pkgs; [ furmark ];
