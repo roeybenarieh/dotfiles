ddd06cfcb80f8549ec3332a3bc8e8af48df0f237
roey ben arieh <roey280404@gmail.com>
2025-04-11 22:04:36 +0300

    feat(hardware): use nixos-hardware flake input for laptop

--- nix/systems/x86_64-linux/laptop/hardware-extra.nix
+++ nix/systems/x86_64-linux/laptop/hardware-extra.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ inputs, ... }:
 
 {
   # better DPI for my laptop screen, help with scaling UI elements
   services.xserver.dpi = 125;
+
+  # nix flake show github:NixOS/nixos-hardware/master | grep common
+  imports = with inputs.nixos-hardware.nixosModules; [
+    common-pc-laptop
+    common-pc-laptop-ssd
+  ];
+  hardware.enableAllFirmware = true;
+
 }
