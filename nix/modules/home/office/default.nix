6ddd9d9e674c115ef021183598bd232fb041a9e7
roey ben arieh <roey280404@gmail.com>
2025-05-11 00:59:31 +0300

    feat: add desktop applications linked to 'Google Docs' and 'Google Sheets'

--- nix/modules/home/office/default.nix
+++ nix/modules/home/office/default.nix
@@ -3,6 +3,31 @@ with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.office;
+
+  powerpoint-icon = pkgs.fetchurl {
+    url = "https://upload.wikimedia.org/wikipedia/commons/0/0d/Microsoft_Office_PowerPoint_%282019%E2%80%93present%29.svg";
+    sha256 = "sha256-rc3WUkFDIqEUpp+Nna7HkXQyMjMvDH7u749pCOXlcGc=";
+  };
+  word-icon = pkgs.fetchurl {
+    url = "https://upload.wikimedia.org/wikipedia/commons/f/fd/Microsoft_Office_Word_%282019%E2%80%93present%29.svg";
+    sha256 = "sha256-oPvT1JtNwPLFrFGAeTiZhdvVCyhPpu2VElwWYLl3u+s=";
+  };
+  powerpoint = pkgs.makeDesktopItem {
+    name = "Microsoft powerpoint";
+    exec = "${config.home.shellAliases.open} https://docs.google.com/presentation/u/0/";
+    desktopName = "Powerpoint";
+    genericName = "Slides/presentation Editor - Google Sheets";
+    categories = [ "Office" "X-Microsoft" ];
+    icon = powerpoint-icon;
+  };
+  word = pkgs.makeDesktopItem {
+    name = "Microsoft word";
+    exec = "${config.home.shellAliases.open} https://docs.google.com/document/u/0";
+    desktopName = "Word";
+    genericName = "Document Editor - Google Docs";
+    categories = [ "Office" "X-Microsoft" ];
+    icon = word-icon;
+  };
 in
 {
   options.${namespace}.office = with types; {
@@ -12,6 +37,8 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       onlyoffice-bin
+      powerpoint
+      word
     ];
   };
 }
