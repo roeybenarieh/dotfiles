1fd03f641a2e9aeb1f2bf25ed57a0007c69acf5e
roey ben arieh <roey280404@gmail.com>
2025-06-22 11:23:46 +0300

    feat(office): provide local Microsoft office 2013 installation

--- nix/modules/home/office/default.nix
+++ nix/modules/home/office/default.nix
@@ -4,6 +4,24 @@ with lib.${namespace};
 let
   cfg = config.${namespace}.office;
 
+  # general staff
+  onedrive-url = "https://onedrive.live.com";
+  exec = "${config.home.shellAliases.open} ${onedrive-url}";
+  categories = [ "Office" "X-Microsoft" ];
+  word-application-name = "Microsoft Word 2013";
+  poweroint-application-name = "Microsoft Powerpoint 2013";
+  word-online-name = "Microsoft Word Online";
+  powerpoint-online-name = "Microsoft Powerpoint Online";
+
+  # functions
+  # NOTE: this wrapper exists because playonlinux is using wine, hence in order for the wine app to get the file it needs to be pre-processed.
+  play_on_linux_exec_wrap = application:
+    "${pkgs.writeShellScript "wrapper" ''
+      #!/bin/sh
+      playonlinux --run "${application}" "$1"
+    ''} %f";
+
+  # assets
   powerpoint-icon = pkgs.fetchurl {
     url = "https://upload.wikimedia.org/wikipedia/commons/0/0d/Microsoft_Office_PowerPoint_%282019%E2%80%93present%29.svg";
     sha256 = "sha256-rc3WUkFDIqEUpp+Nna7HkXQyMjMvDH7u749pCOXlcGc=";
@@ -12,23 +30,25 @@ let
     url = "https://upload.wikimedia.org/wikipedia/commons/f/fd/Microsoft_Office_Word_%282019%E2%80%93present%29.svg";
     sha256 = "sha256-oPvT1JtNwPLFrFGAeTiZhdvVCyhPpu2VElwWYLl3u+s=";
   };
-  onedrive-url = "https://onedrive.live.com";
-  powerpoint = pkgs.makeDesktopItem {
-    name = "Microsoft powerpoint";
-    exec = "${config.home.shellAliases.open} ${onedrive-url}";
-    desktopName = "Powerpoint";
-    genericName = "Slides/presentation Editor - Google Sheets";
-    categories = [ "Office" "X-Microsoft" ];
+
+  # desktop items
+  powerpoint-online = pkgs.makeDesktopItem {
+    name = powerpoint-online-name;
+    inherit exec;
+    desktopName = powerpoint-online-name;
+    genericName = "Slides/presentation Editor";
+    inherit categories;
     icon = powerpoint-icon;
   };
-  word = pkgs.makeDesktopItem {
-    name = "Microsoft word";
-    exec = "${config.home.shellAliases.open} ${onedrive-url}";
-    desktopName = "Word";
-    genericName = "Document Editor - Google Docs";
-    categories = [ "Office" "X-Microsoft" ];
+  word-online = pkgs.makeDesktopItem {
+    name = word-online-name;
+    inherit exec;
+    desktopName = word-online-name;
+    genericName = "Document Editor";
+    inherit categories;
     icon = word-icon;
   };
+
 in
 {
   options.${namespace}.office = with types; {
@@ -36,10 +56,37 @@ in
   };
 
   config = mkIf cfg.enable {
+    xdg.desktopEntries = {
+      ${word-application-name} = {
+        name = word-application-name;
+        exec = play_on_linux_exec_wrap word-application-name;
+        icon = word-icon;
+        mimeType = [ "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ];
+        # startupNotify = true;
+        terminal = false;
+        inherit categories;
+      };
+      ${poweroint-application-name} = {
+        name = poweroint-application-name;
+        exec = play_on_linux_exec_wrap poweroint-application-name;
+        icon = powerpoint-icon;
+        mimeType = [ "application/vnd.openxmlformats-officedocument.presentationml.presentation" ];
+        # startupNotify = true;
+        terminal = false;
+        inherit categories;
+      };
+    };
     home.packages = with pkgs; [
-      onlyoffice-bin
-      powerpoint
-      word
+      # onlyoffice-bin
+      powerpoint-online
+      word-online
+
+      # TODO: make the installation declaritevly + set mime types
+      # before watching windows below, mount .iso file, there you would find the setup.exe file.
+      # NOTE: the iso is commited to this repository because after a few mounths the .iso installation URLs are taken down
+      # microsoft office installation instructions: https://www.youtube.com/watch?v=LH-6tp-KBuQ&t=66s
+      playonlinux
+      samba
     ];
   };
 }
