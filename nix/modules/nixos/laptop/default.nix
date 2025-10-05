bf137c145ad980bb5da9c66eb60a4c3fbd47cd72
Roey Ben Arieh <roey280404@gmail.com>
2025-10-05 10:30:18 +0300

    fix(laptop): use libinput-gestures

--- nix/modules/nixos/laptop/default.nix
+++ nix/modules/nixos/laptop/default.nix
@@ -1,4 +1,4 @@
-{ namespace, lib, config, ... }:
+{ pkgs, namespace, lib, config, ... }:
 
 with lib;
 with lib.${namespace};
@@ -20,6 +20,37 @@ in
         disableWhileTyping = true;
       };
     };
+
+    environment = {
+      # configuration docs at https://github.com/bulletmark/libinput-gestures/blob/master/libinput-gestures.conf
+      # FIX: in firefox/chromium: when swiping 3 fingers to the right it goes backwards and vise versa(against natural logic)
+      # etc."libinput-gestures.conf".text = ''
+      #   gesture swipe right 3 ${getExe pkgs.bash} -c '
+      #     user=$(${pkgs.systemd}/bin/loginctl show-user $(${pkgs.systemd}/bin/loginctl | awk "/seat0/ {print \$1}") -p Name --value)
+      #     export DISPLAY=:0
+      #     export XAUTHORITY=/home/$user/.Xauthority
+      #     DISPLAY=:0 XAUTHORITY=/home/$user/.Xauthority ${getExe pkgs.xdotool} key --clearmodifiers ctrl+Tab
+      #   '
+      # '';
+
+      systemPackages = with pkgs; [
+        libinput-gestures
+        wmctrl # needed by libinput-gestures to switch workspaces
+      ];
+    };
+
+    systemd.services.libinput-gestures = {
+      description = "Libinput Gestures Service";
+      after = [ "graphical.target" ];
+      wantedBy = [ "graphical.target" ];
+
+      serviceConfig = {
+        ExecStart = "${getExe pkgs.libinput-gestures} -c /etc/libinput-gestures.conf";
+        Restart = "always";
+        User = "root";
+      };
+    };
+
 
     # enable bluetooth
     hardware.bluetooth.enable = true;
