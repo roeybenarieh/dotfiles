6881391208c179e8fa44a662eaefb6b89b89a214
Roey Ben Arieh <roey280404@gmail.com>
2025-12-22 11:22:21 +0200

    feat(observability): handle OTLP in grafana alloy

--- nix/modules/nixos/observability/opentelemetry/default.nix
+++ nix/modules/nixos/observability/opentelemetry/default.nix
@@ -9,6 +9,7 @@ in
 {
   options.${namespace}.observability.opentelemetry = with types; {
     enable = mkBoolOpt false "Whether or not to enable OpenTelemetry related products.";
+    otelp_grpc_port = mkIntOpt 4317 "port to run the otelp grpc receiver";
   };
 
   config = mkIf cfg.enable {
@@ -21,6 +22,36 @@ in
     services.alloy = {
       enable = true;
       configPath = pkgs.writeText "config.alloy" ''
+        // ===============================
+        //        RECEIVERS (OTLP)
+        // ===============================
+        otelcol.receiver.otlp "default" {
+          grpc { endpoint = "0.0.0.0:${toString cfg.otelp_grpc_port}" }
+          output {
+            metrics = [otelcol.processor.batch.default.input]
+            logs    = [otelcol.processor.batch.default.input]
+            traces  = [otelcol.processor.batch.default.input]
+          }
+        }
+        otelcol.processor.batch "default" {
+          output {
+            metrics = [otelcol.exporter.prometheus.default.input]
+            logs    = [otelcol.exporter.loki.default.input]
+            traces  = [otelcol.exporter.otlp.default.input]
+          }
+        }
+
+        // ===============================
+        //        EXPORTERS (OTLP)
+        // ===============================
+        otelcol.exporter.loki "default" {
+          forward_to = [loki.write.loki_push.receiver]
+        }
+        otelcol.exporter.prometheus "default" {
+          forward_to = [prometheus.remote_write.thanos.receiver]
+        }
+
+
         // ================================
         //   FIND LOGS FROM JOURNAL
         // ==============================
@@ -33,29 +64,36 @@ in
           }
         }
         loki.source.journal "read"  {
-          forward_to    = [loki.process.logs.receiver]
+          forward_to    = [loki.write.loki_push.receiver]
           relabel_rules = loki.relabel.journal.rules
           labels        = {component = "loki.source.journal"}
         }
 
         // ================================
-        //   PROCESS LOGS - add 'host' label
+        //            SEND DATA
         // ================================
-        loki.process "logs" {
-          stage.labels {
-            values = {
-              host = "${config.networking.hostName}",
-            }
-          }
-          forward_to = [loki.write.loki_push.receiver]
-        }
-
-        // ================================
-        //   SEND TO LOKI
-        // ================================
+        // Loki
         loki.write "loki_push" {
           endpoint {
             url = "http://localhost:3100/loki/api/v1/push"
+          }
+          external_labels = {
+            host = "${config.networking.hostName}",
+          }
+        }
+        // Prometheus
+        prometheus.remote_write "thanos" {
+          endpoint {
+            url = "http://localhost:11908/api/v1/receive"
+          }
+        }
+        // OTLP
+        otelcol.exporter.otlp "default" {
+          client {
+            endpoint = "localhost:11907"
+            tls {
+              insecure = true
+            }
           }
         }
 
