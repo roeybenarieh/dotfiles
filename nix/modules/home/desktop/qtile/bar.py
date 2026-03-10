2a0f9ffdc5f6e7f31db9d5b5bc984773bce009fb
Roey Ben Arieh <roey280404@gmail.com>
2026-03-10 18:28:23 +0200

    Revert "fix(qtile): cleanup"
    This reverts commit 5f5e95b9aef553d1d3a0ed777b96bb6805a5872c.

--- nix/modules/home/desktop/qtile/bar.py
+++ nix/modules/home/desktop/qtile/bar.py
@@ -1,10 +1,11 @@
 import re
 import subprocess
 
+from keyboard_layout_widget import MyKeyboardLayout
 from libqtile import bar, widget
 from libqtile.widget.nvidia_sensors import NvidiaSensors
 from libqtile.lazy import lazy
-from libqtile.widget import PulseVolume, KeyboardLayout
+from libqtile.widget import PulseVolume
 
 import colors
 import dexcom_widget
@@ -24,7 +25,11 @@ DexcomGlucose = mk_overrides(
     update_interval=10,  # update every 10s
 )
 
-KeyboardLayout = mk_overrides(KeyboardLayout, configured_keyboards=["us", "il"])
+KeyboardLayout = mk_overrides(
+    MyKeyboardLayout,
+    text="UNK",
+    update_interval=0.1,
+)
 
 # TODO: get gpu usage%
 NvidiaTemp = mk_overrides(NvidiaSensors, format="{temp}°C", update_interval=5)
@@ -37,10 +42,8 @@ DexcomInRangePercentage = mk_overrides(
 Volume = mk_overrides(
     PulseVolume,
     mouse_callbacks={
-        "Button1": lazy.spawn(SETTINGS.auidio_controller),  # left click
-        "Button2": lazy.spawn(SETTINGS.audio_visualizer),  # wheel click
-        "Button4": lazy.spawn("pamixer --increase 5"),  # Scroll Up
-        "Button5": lazy.spawn("pamixer --decrease 5"),  # Scroll Down
+        "Button2": lazy.spawn(SETTINGS.auidio_controller),
+        "Button3": lazy.spawn(SETTINGS.audio_visualizer),
     },
 )
 
