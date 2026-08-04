3d3c3849f9320e2cd3a6465fce8fc9c5de41afdd
Roey Ben Arieh <roey280404@gmail.com>
2026-08-04 10:37:42 +0300

    feat(hyprland): add system commands to applicaton launcher
    i.e. restart, shutdown, hibernate...

--- nix/modules/home/desktop/hyprland/application-launcher/default.nix
+++ nix/modules/home/desktop/hyprland/application-launcher/default.nix
@@ -5,6 +5,8 @@ with lib.${namespace};
 let
   cfg = config.${namespace}.desktop.hyprland.applicationLauncher;
   lua = lib.generators.mkLuaInline;
+  papirus = pkgs.papirus-icon-theme;
+  icon = name: "${papirus}/share/icons/Papirus/48x48/apps/${name}.svg";
 in
 {
   options.${namespace}.desktop.hyprland.applicationLauncher = with types; {
@@ -14,10 +16,35 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ rofi ];
 
-    xdg.configFile."rofi/applications-config.rasi".source = ../rofi/applications-config.rasi;
+    xdg.desktopEntries = {
+      power-shutdown = {
+        name = "Shutdown";
+        exec = "systemctl poweroff";
+        icon = icon "system-shutdown";
+        categories = [ "System" ];
+      };
+      power-restart = {
+        name = "Restart";
+        exec = "systemctl reboot";
+        icon = icon "system-reboot";
+        categories = [ "System" ];
+      };
+      power-hibernate = {
+        name = "Hibernate";
+        exec = "systemctl hibernate";
+        icon = icon "system-hibernate";
+        categories = [ "System" ];
+      };
+      power-lock-screen = {
+        name = "Lock Screen";
+        exec = "hyprlock";
+        icon = icon "system-lock-screen";
+        categories = [ "System" ];
+      };
+    };
 
     wayland.windowManager.hyprland.settings.bind = [
-      { _args = [ "SUPER + Super_L" (lua ''hl.dsp.exec_cmd("${pkgs.rofi}/bin/rofi -show drun -config ${../rofi/applications-config.rasi}")'') { release = true; } ]; }
+      { _args = [ "SUPER + Super_L" (lua ''hl.dsp.exec_cmd("${pkgs.rofi}/bin/rofi -show drun -config ${./applications-config.rasi}")'') { release = true; } ]; }
     ];
   };
 }
